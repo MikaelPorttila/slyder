149010e5f8bc35fda73753a7e24829632c4e7c6e
Mikael Porttila <mikael.porttila@gmail.com>
2021-08-23 23:56:13 +0200

    Scroll selected slide into view

--- packages/client/src/components/Slide.tsx
+++ packages/client/src/components/Slide.tsx
@@ -1,13 +1,22 @@
 import type { Component } from "solid-js"
-import { Switch, Match } from "solid-js"
+import { Switch, Match, createEffect } from "solid-js"
 import { getPresentationContext } from "../context/presentation";
 import styles from "./Slide.module.css";
 import type { SlideProps } from "./types/slide-props";
 
 export const Slide: Component<SlideProps> = ({ slide }) => {
     const [state, {setCurrentSlide}] = getPresentationContext();
+    let slideElement;
+
+    createEffect(() => {
+        if(state?.currentSlide?.fileName === slide.fileName && slideElement) {
+            (slideElement as any).scrollIntoViewIfNeeded();
+        }
+    })
+
     return (
         <button 
+            ref={slideElement}
             type="button"
             class={styles.slide}
             classList={{ [styles.active]: state?.currentSlide?.fileName === slide.fileName }}
